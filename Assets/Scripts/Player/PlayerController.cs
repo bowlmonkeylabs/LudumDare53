327e6b5ef09b9355f60f5fa29de35881b7862287
Dan C <djc5627@gmail.com>
2023-04-29 15:36:23 -0400

    Implement net gun and basic interaction system.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BML.Scripts;
+using BML.Scripts.UI;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField, FoldoutGroup("References")] private Transform _mainCamera;
+
+        [SerializeField, FoldoutGroup("Interact")] private float _interactDistance = 5f;
+        [SerializeField, FoldoutGroup("Interact")] private float _interactCastRadius = .25f;
+        [SerializeField, FoldoutGroup("Interact")] private LayerMask _interactMask;
+
+        [SerializeField, FoldoutGroup("NetGun")] private bool _hasNetGun;
+        [SerializeField, FoldoutGroup("NetGun")] private GameObject _netPrefab;
+        [SerializeField, FoldoutGroup("NetGun")] private Transform _netContainer;
+        [SerializeField, FoldoutGroup("NetGun")] private float _netGunCooldown = .3f;
+        [SerializeField, FoldoutGroup("NetGun")] private float _netGunForce = 1000f;
+
+        private float lastFireNetTime = Mathf.NegativeInfinity;
+
+        #region Input Callback
+
+        private void OnPrimary(InputValue value)
+        {
+            if (!value.isPressed) return;
+            
+            if (_hasNetGun)
+                TryFireNetGun();
+            else
+                TryInteract();
+        }
+
+        #endregion
+
+        private void TryFireNetGun()
+        {
+            if (lastFireNetTime + _netGunCooldown > Time.time)
+                return;
+
+            GameObject net = Instantiate(_netPrefab, _mainCamera.position, _mainCamera.rotation, _netContainer);
+            net.GetComponent<Rigidbody>().AddForce(_netGunForce * _mainCamera.forward);
+            lastFireNetTime = Time.time;
+        }
+
+        private void TryInteract()
+        {
+            RaycastHit hit;
+            
+            if (Physics.SphereCast(_mainCamera.position, _interactCastRadius, _mainCamera.forward, out hit,
+                _interactDistance, _interactMask, QueryTriggerInteraction.Ignore))
+            {
+                InteractionReceiver interactionReceiver = hit.collider.GetComponent<InteractionReceiver>();
+                if (interactionReceiver == null)
+                    return;
+
+                interactionReceiver.ReceiveInteraction();
+            }
+        }
+        
+    }
+}
