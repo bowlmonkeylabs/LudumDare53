2c831409ca261dbfa8c29e8626f6ba54664f4085
merc37 <gabeajh@gmail.com>
2023-04-29 23:50:50 -0400

    Remove unused audio mixer vars

--- Assets/Scripts/Audio/AudioMixerController.cs
+++ Assets/Scripts/Audio/AudioMixerController.cs
@@ -10,49 +10,27 @@ namespace BML.Scripts
     {
         [SerializeField] private AudioMixer _audioMixer;
         [SerializeField] private FloatReference _settingMasterVolume;
-        [SerializeField] private FloatReference _settingMusicVolume;
-        [SerializeField] private FloatReference _settingSfxVolume;
 
         private void Start()
         {
             SetMasterVolume(0f, _settingMasterVolume.Value);
-            SetMusicVolume(0f, _settingMusicVolume.Value);
-            SetSfxVolume(0f, _settingSfxVolume.Value);
         }
 
         private void OnEnable()
         {
             SetMasterVolume(0f, _settingMasterVolume.Value);
-            SetMusicVolume(0f, _settingMusicVolume.Value);
-            SetSfxVolume(0f, _settingSfxVolume.Value);
             _settingMasterVolume.Subscribe(SetMasterVolume);
-            _settingMusicVolume.Subscribe(SetMusicVolume);
-            _settingSfxVolume.Subscribe(SetSfxVolume);
         }
         
         private void OnDisable()
         {
             _settingMasterVolume.Unsubscribe(SetMasterVolume);
-            _settingMusicVolume.Unsubscribe(SetMusicVolume);
-            _settingSfxVolume.Unsubscribe(SetSfxVolume);
         }
 
         private void SetMasterVolume(float previousVolume, float newVolume)
         {
             float volume = ProcessVolume(newVolume);
             _audioMixer.SetFloat("MasterVolume", volume);
-        }
-
-        private void SetMusicVolume(float previousVolume, float newVolume)
-        {
-            float volume = ProcessVolume(newVolume);
-            _audioMixer.SetFloat("MusicVolume", volume);
-        }
-
-        private void SetSfxVolume(float previousVolume, float newVolume)
-        {
-            float volume = ProcessVolume(newVolume);
-            _audioMixer.SetFloat("SfxVolume", volume);
         }
 
         private float ProcessVolume(float volume)
