f8ae4fab0640809c981cdf9f6ce4943d85a7b669
Max Lepkowski <mlepkows@stevens.edu>
2023-05-01 12:56:41 -0400

    Add post-process effect for caffeine.

---
+++ Assets/Scripts/CaffeinatedEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace DefaultNamespace
+{
+    public class CaffeinatedEffect : MonoBehaviour
+    {
+        [SerializeField] private BoolReference IsCaffeinated;
+        [SerializeField] private PostProcessVolume volume;
+
+        private void Update()
+        {
+            if (IsCaffeinated.Value)
+            {
+                volume.weight = 1f;
+            }
+            else
+            {
+                volume.weight = 0f;
+            }
+        }
+    }
+}
