7e2f5f49a7e7298ecb07b649e2ec351ab4100d6f
Dan C <djc5627@gmail.com>
2023-04-30 22:59:12 -0400

    Add first person camera for net gun.

---
+++ Assets/Scripts/UI/SetCanvasCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BML.Scripts.UI
+{
+    public class SetCanvasCamera : MonoBehaviour
+    {
+        [SerializeField] private Canvas _canvas;
+        [SerializeField] private Camera _camera;
+
+        private void Start()
+        {
+            _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            _canvas.worldCamera = _camera;
+        }
+    }
+}
